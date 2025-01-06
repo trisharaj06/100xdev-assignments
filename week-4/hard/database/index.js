b6ba4a97b4772a52de8396d5b30c70321e8e6ccb
trisharaj06 <trirox2030@gmail.com>
2025-01-06 23:51:57 +0530

    added user schema, completed 2 ep

--- week-4/hard/database/index.js
+++ week-4/hard/database/index.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
-
+require("dotenv").config()
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_URI);
 
 // Define schemas
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 const TodoSchema = new mongoose.Schema({
     // Schema definition here
+    title: String
 });
 
 const User = mongoose.model('User', UserSchema);
--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -1,14 +1,66 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const User = require("../database/index")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement user signup logic
+    const {email, password} = req.body;
+
+    try{
+        const userAlreadyPresent  = await User.findOne({email})
+        if(userAlreadyPresent){
+            res.status(409).json({
+                message: "User with this email already exists"
+            })
+        }
+        const hashpwd = await bcrypt.hash(password,12);
+        await User.create({email, password: hashpwd})
+
+        res.status(201).json({
+            message: "You rae signed up successfully"
+        })
+    }catch(err){
+        res.status(500).json({
+            message: "Error while signing up"
+        })
+    }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
      // Implement user login logic
+     const {email, password} = req.body;
+
+     try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            res.status(404).json({
+                message: "User not found, Please signup first"
+            })
+        }
+
+        const pwdCheck = await bcrypt.compare(password,this.user.password);
+
+        if(!pwdCheck){
+            res.status(403).json({
+                message: "Incorrect password"
+            })
+        }
+
+        const token = jwt.sign({id: user._id},process.env.SECRET)
+
+        res.json({
+            message: "Logged in successfully", token
+        })
+     }catch(err){
+        res.status(500).json({
+            message: "Error while logging in"
+        })
+
+     }
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
